d9fab606206a46e83ec29032d779a106064775fc
DELL-3\User <darinazhivkova>
2025-10-17 14:21:35 +0300

    added section 2

--- src/app/sections/+sections/sections.ts
+++ src/app/sections/+sections/sections.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Section1 } from "../section-1/section-1";
+import { Section2 } from "../section-2/section-2";
 
 @Component({
   selector: 'app-sections',
-  imports: [Section1],
+  imports: [Section1, Section2],
   templateUrl: './sections.html',
   styleUrl: './sections.scss'
 })
---
+++ src/app/sections/section-2/section-2.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card4 } from '../../cards/card-4/card-4';
+
+@Component({
+  selector: 'app-section-2',
+  imports: [Card4],
+  templateUrl: './section-2.html',
+  styleUrl: './section-2.scss'
+})
+export class Section2 {
+
+}
