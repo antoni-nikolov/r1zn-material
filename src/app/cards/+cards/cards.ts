f7449291b3534ce524d754f2949506c3bac5e4e3
DELL-3\User <darinazhivkova>
2025-10-17 13:31:33 +0300

    added card-4

--- src/app/cards/+cards/cards.ts
+++ src/app/cards/+cards/cards.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { Card1 } from '../card-1/card-1';
 import { Card2 } from '../card-2/card-2';
 import { Card3 } from '../card-3/card-3';
+import { Card4 } from "../card-4/card-4";
 
 @Component({
   selector: 'app-cards',
   imports: [
     Card1,
     Card2,
-    Card3
-  ],
+    Card3,
+    Card4
+],
   templateUrl: './cards.html',
   styleUrl: './cards.scss'
 })
---
+++ src/app/cards/card-4/card-4.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-4',
+  imports: [],
+  templateUrl: './card-4.html',
+  styleUrl: './card-4.scss'
+})
+export class Card4 {
+
+}
